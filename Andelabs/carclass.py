02aab5256ff236fdb5b8308900cdf5a3885fc608
Ruth Bochere <rorbochere@gmail.com>
2016-04-29 18:02:46 +0300

    Andela commit

--- Andelabs/carclass.py
+++ Andelabs/carclass.py
@@ -1,42 +1,41 @@
 class Car(object):
-	def __init__(self, *args):
-		self.car_type = car_type
-		self.car_name = car_name 
-		self.car_model = car_model
-
-		if car_name == (""):
-			return car_name == "General"
-		return car_name
-
-		if car_model == (""):
-			return car_model == "GM"
-		return car_model
-
-		if car_name == "Porsche": 
-			num_of_doors = 2
-		elif car_name == "Koenigsegg":
-			num_of_doors = 2
-		num_of_doors = 4
-		
-		if car_type == "trailer": 
-			num_of_wheels = 8
-		num_of_wheels = 4  
-		
-		if car_type == "trailer":
-			car_type = "trailer"
-			parked_speed = "0 km/h"
-			moving_speed = "the pedal to the metal"
-		car_type = "saloon"
-
-		if car_name == "Mercedes":
-			parked_speed = "0 km\h"
-			moving_speed = "the pedal to the metal"
-
-	def car_drive(self):
-		pass
-
-
-honda = Car("Honda", )
-
-
-#print honda
+	
+	def __init__(self, *args): 
+	  
+	  if len(args) == 0:
+	    self.name = 'General'
+	    self.model = 'GM'
+	    
+	  if len(args) >= 1:
+	    self.name = args[0]
+	    
+	  if len(args) >= 2:
+	    self.model = args[1]
+	    
+	  if self.name in ('Porshe', 'Koenigsegg'): 
+	     self.num_of_doors = 2
+	  else:
+	     self.num_of_doors = 4
+	     
+	  if len(args) >= 3:
+	    self.car_type = args[2]
+	  else:
+	    self.car_type = 'saloon'
+	    
+	  if self.car_type == 'trailer':
+	    self.num_of_wheels = 8
+	  else:
+	   self.num_of_wheels = 4
+	   
+	  self.speed = 0
+	   
+	def is_saloon(self):
+	  return self.car_type == 'saloon'
+	  
+	def drive(self, spd):
+	  if self.car_type == 'trailer':
+	    self.speed = spd * 11
+	  else:
+	    self.speed = 10 ** spd
+	    
+	  return self
--- Andelabs/wordcount.py
+++ Andelabs/wordcount.py
@@ -1,13 +1,33 @@
-wordslist = wordslist.split()
+# wordslist = wordslist.split()
 
-dict_ = {}
+# dict_ = {}
 
-for word in wordslist:
-    if word.isdigit():
-        word = int(word)
-    if word in dict_:
-        dict_[word] = dict_[word] + 1
-    else:
-        dict_[word] = 1
+# for word in wordslist:
+#     if word.isdigit():
+#         word = int(word)
+#     if word in dict_:
+#         dict_[word] = dict_[word] + 1
+#     else:
+#         dict_[word] = 1
 
-return dict_
+# return dict_
+
+# split() returns a list of all the words in a
+# string, using specified str or whitespace as 
+# separator  
+
+def words(sentence):
+    sentence = sentence.split()
+    # splits a string of items in a sentence words
+    dict_ = {} # initializing empty dict
+    # empty dict is used for storing the recurring numbers 
+    for word in sentence: # loops through content of sentence
+        if word.isdigit(): # isdigit() checks if a string 
+        # consists of digits only
+            word = int(word) # counts digit as one word 
+        if word in dict_: # loop for adding words into dictionary
+            dict_[word] = dict_[word] + 1
+        else:
+            dict_[word] = 1
+
+    return dict_
