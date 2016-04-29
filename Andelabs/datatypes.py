ef7c1ca2510e774a898c117855d75263ed0088ff
Ruth Bochere <rorbochere@gmail.com>
2016-04-29 08:46:24 +0300

    Andelabs commit

---
+++ Andelabs/datatypes.py
@@ -0,0 +1,34 @@
+def data_type(A=None):
+	if type(A) == str:
+		return len(A)
+	if type(A) == None:
+		return "no value"  
+	if type(A) == bool:
+		return A
+	if type(A) == int: 
+		if A == 100:
+			return "equal to 100"
+		elif A < 100:
+			return "less than 100"
+		return "more than 100"
+	if type(A) == list:
+		if len(A) >= 3:
+			return A[2]
+		return "None"
+
+print data_type()
+print data_type([1, 2, 3])
+print data_type([1, 2])
+print data_type(True)
+print data_type(3)
+print data_type(100)
+print data_type(200)
+print data_type("andela")
+
+
+
+
+
+
+
+
--- Andelabs/factorial.py
+++ Andelabs/factorial.py
@@ -2,7 +2,8 @@ def factorial(number):
 	if number <= 1:
 		return 1
 	else:
-		number = number * factorial(number - 1)
+		number = number * factorial(number - 1) 
+		# Function calls itself here, making this a recursive function
 	return number
 
 print factorial(2)
